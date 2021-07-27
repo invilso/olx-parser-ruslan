819e294bcf36adcb97fadcc9a09564677401fd48
invilso <40578604+invilso@users.noreply.github.com>
2021-07-27 19:39:06 +0300

    add requirements.txt and page_loop

--- parse.py
+++ parse.py
@@ -1,3 +1,10 @@
 import requests
 
-print(requests.get('https://www.olx.ua/elektronika/').text)
+class Parser():
+    max_page = 1
+    current_page = 1
+    def pages_loop(self):
+        while self.max_page >= self.current_page:
+            requests.get('https://www.olx.ua/elektronika/?page=2')
+
+class Writer():
